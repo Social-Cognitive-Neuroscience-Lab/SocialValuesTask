1083c97e6b6c293bf847adcf36c16c9ce4e992ea
kjyoder <kjyoder@gmail.com>
2021-09-29 15:22:18 -0500

    updated issuelist.js with For/Against photos

--- issuelist.js
+++ issuelist.js
@@ -1,162 +1,260 @@
 var issue_list = [
   {
     "Short": "Confederate",
-    "Issue": "Remove public confederate memorials"
+    "Issue": "Remove public confederate memorials",
+    "For": "Photo_1050.png",
+    "Against": "Photo_1001.png"
   },
   {
     "Short": "BLM",
-    "Issue": "Black Lives Matter"
+    "Issue": "Black Lives Matter",
+    "For": "Photo_1034_01.png",
+    "Against": "Photo_1027_02.png"
   },
   {
     "Short": "AltRight",
-    "Issue": "Allow alt-right speakers on campuses"
+    "Issue": "Allow alt-right speakers on campuses",
+    "For": "Photo_262.png",
+    "Against": "Photo_1021_01.png"
   },
   {
     "Short": "MandatoryConsent",
-    "Issue": "Mandatory training on sexual consent"
+    "Issue": "Mandatory training on sexual consent",
+    "For": "Photo_275.png",
+    "Against": "Photo_1047_01.png"
   },
   {
     "Short": "BorderWall",
-    "Issue": "Build southern border wall"
+    "Issue": "Build southern border wall",
+    "For": "Photo_282.png",
+    "Against": "Photo_258.png"
   },
   {
     "Short": "MigrantChildren",
-    "Issue": "Detention of migrant children"
+    "Issue": "Detention of migrant children",
+    "For": "Photo_1001.png",
+    "Against": "Photo_301.png"
   },
   {
     "Short": "EthnicHalloween",
-    "Issue": "Ban on ethnic halloween costumes"
+    "Issue": "Ban on ethnic halloween costumes",
+    "For": "Photo_362.png",
+    "Against": "Photo_276.png"
   },
   {
     "Short": "MandatoryVaccine",
-    "Issue": "Mandatory vaccinations"
+    "Issue": "Mandatory vaccinations",
+    "For": "Photo_1003_01.png",
+    "Against": "Photo_280.png"
   },
   {
     "Short": "BanAnimalResearch",
-    "Issue": "Ban on research with animals"
+    "Issue": "Ban on research with animals",
+    "For": "Photo_1016.png",
+    "Against": "Photo_1020_01.png"
   },
   {
     "Short": "ReligExempt",
-    "Issue": "Religious exemptions for discrimination laws"
+    "Issue": "Religious exemptions for discrimination laws",
+    "For": "Photo_320.png",
+    "Against": "Photo_1021_02.png"
   },
   {
     "Short": "AbortionRights",
-    "Issue": "Abortion rights"
+    "Issue": "Abortion rights",
+    "For": "Photo_316.png",
+    "Against": "Photo_1080.png"
   },
   {
     "Short": "ClimateLegislation",
-    "Issue": "Climate change legislation"
+    "Issue": "Climate change legislation",
+    "For": "Photo_1014.png",
+    "Against": "Photo_1047_02.png"
   },
   {
     "Short": "AffirmativeAction",
-    "Issue": "Affirmative action"
+    "Issue": "Affirmative action",
+    "For": "Photo_272.png",
+    "Against": "Photo_337.png"
   },
   {
     "Short": "DormsGender",
-    "Issue": "Dorms determined by gender identity"
+    "Issue": "Dorms determined by gender identity",
+    "For": "Photo_254.png",
+    "Against": "Photo_369.png"
   },
   {
     "Short": "ParksBudget",
-    "Issue": "Cuts to national parks budget"
+    "Issue": "Cuts to national parks budget",
+    "For": "Photo_1007_01.png",
+    "Against": "Photo_273.png"
   },
   {
     "Short": "TriggerWarnings",
-    "Issue": "Mandatory trigger warningns"
+    "Issue": "Mandatory trigger warnings",
+    "For": "Photo_367.png",
+    "Against": "Photo_1020_02.png"
   },
   {
     "Short": "TuitionIncrease",
-    "Issue": "Tuition increases"
+    "Issue": "Tuition increases",
+    "For": "Photo_341.png",
+    "Against": "Photo_1075.png"
   },
   {
     "Short": "WarDrugs",
-    "Issue": "War on drugs"
+    "Issue": "War on drugs",
+    "For": "Photo_1068_01.png",
+    "Against": "Photo_300.png"
   },
   {
     "Short": "CampusCarry",
-    "Issue": "On campus concealed carry"
+    "Issue": "On campus concealed carry",
+    "For": "Photo_1005_02.png",
+    "Against": "Photo_999.png"
   },
   {
     "Short": "DisarmCampus",
-    "Issue": "Gun free campus police"
+    "Issue": "Gun free campus police",
+    "For": "Photo_202.png",
+    "Against": "Photo_377.png"
   },
   {
     "Short": "AbolishPrison",
-    "Issue": "Abolishing private prisons"
+    "Issue": "Abolishing private prisons",
+    "For": "Photo_374.png",
+    "Against": "Photo_220.png"
   },
   {
     "Short": "StudentUnionization",
-    "Issue": "Student worker unionization"
+    "Issue": "Student worker unionization",
+    "For": "Photo_217.png",
+    "Against": "Photo_1072.png"
   },
   {
     "Short": "AbolishFrats",
-    "Issue": "Abolishing fraternities"
+    "Issue": "Abolishing fraternities",
+    "For": "Photo_257.png",
+    "Against": "Photo_206.png"
   },
   {
     "Short": "BathroomBioSex",
-    "Issue": "Bathrooms restricted based on biological sex"
+    "Issue": "Bathrooms restricted based on biological sex",
+    "For": "Photo_230.png",
+    "Against": "Photo_1082.png"
   },
   {
     "Short": "BDSIsrael",
-    "Issue": "BDS israel"
+    "Issue": "Boycott, Divest, and Sanction Israel",
+    "For": "Photo_1038.png",
+    "Against": "Photo_1081.png"
   },
   {
     "Short": "WelfareDrugTest",
-    "Issue": "Mandatory drug tests for welfare recipients"
+    "Issue": "Mandatory drug tests for welfare recipients",
+    "For": "Photo_1034_02.png",
+    "Against": "Photo_261.png"
   },
   {
     "Short": "ExpelWhiteNationalists",
-    "Issue": "Expel students with white nationlist views"
+    "Issue": "Expel students with white nationlist views",
+    "For": "Photo_1077.png",
+    "Against": "Photo_228.png"
   },
   {
-    "Short": "YemenDrone",
-    "Issue": "U.S. drone strikes in Yemen"
+    "Short": "Drones",
+    "Issue": "U.S. drone strikes in Afghanistan",
+    "For": "Photo_1065_01.png",
+    "Against": "Photo_1019_01.png"
   },
   {
     "Short": "JerusalemCapital",
-    "Issue": "Recognize Jerusalem as capital of Israel"
+    "Issue": "Recognize Jerusalem as capital of Israel",
+    "For": "Photo_297.png",
+    "Against": "Photo_1008_01.png"
   },
   {
     "Short": "LegalizeMarijuana",
-    "Issue": "Legalization of marijuana"
+    "Issue": "Legalization of marijuana",
+    "For": "Photo_333.png",
+    "Against": "Photo_225.png"
   },
   {
     "Short": "DefundPlannedParenthood",
-    "Issue": "Defund planned parenthood"
+    "Issue": "Defund Planned Parenthood",
+    "For": "Photo_268.png",
+    "Against": "Photo_218.png"
   },
   {
     "Short": "InjectionSites",
-    "Issue": "Safe injection locations"
+    "Issue": "Safe injection locations",
+    "For": "Photo_1079.png",
+    "Against": "Photo_1076_02.png"
   },
   {
     "Short": "HardenSchools",
-    "Issue": "Hardening schools/Arming teachers"
+    "Issue": "Hardening schools/Arming teachers",
+    "For": "Photo_1008_02.png",
+    "Against": "Photo_1002_02.png"
   },
   {
     "Short": "Mask",
-    "Issue": "Against mask mandates"
+    "Issue": "Mask mandates",
+    "For": "Photo_1002_01.png",
+    "Against": "Photo_1007_02.png"
   },
   {
     "Short": "SchoolPolice",
-    "Issue": "Remove police from schools"
-  },
-  {
-    "Short": "Graffiti",
-    "Issue": "Racist Graffit on campus"
+    "Issue": "Remove police from schools",
+    "For": "Photo_1017_01.png",
+    "Against": "Photo_1001.png"
   },
   {
     "Short": "Euthanasia",
-    "Issue": "Allow terminally ill patients to terminate their lives"
+    "Issue": "Physician-assisted suicide",
+    "For": "Photo_368.png",
+    "Against": "Photo_1004.png"
   },
   {
     "Short": "Sanctuary",
-    "Issue": "University Campus should be a Satuary"
-  },
-  {
-    "Short": "UnivPresResign",
-    "Issue": "University President should resign"
+    "Issue": "University Campus should be a Sanctuary",
+    "For": "Photo_1078.png",
+    "Against": "Photo_1006_02.png"
   },
   {
     "Short": "DefundPolice",
-    "Issue": "Defund police departments"
+    "Issue": "Defund the police",
+    "For": "Photo_1024_01.png",
+    "Against": "Photo_1083.png"
+  },
+  {
+    "Short": "GunControl",
+    "Issue": "Increased Gun regulations",
+    "For": "Photo_1014.png",
+    "Against": "Photo_1026_01.png"
+  },
+  {
+    "Short": "COVIDLockdown",
+    "Issue": "Lift COVID lockdowns",
+    "For": "Photo_1084.png",
+    "Against": "Photo_335.png"
+  },
+  {
+    "Short": "ColumbusStatue",
+    "Issue": "Remove Christopher Columbus statue",
+    "For": "Photo_1045_02.png",
+    "Against": "Photo_1032.png"
+  },
+  {
+    "Short": "PoliceBrut",
+    "Issue": "Stop police brutality",
+    "For": "Photo_1035_02.png",
+    "Against": "Photo_231.png"
+  },
+  {
+    "Short": "MinimumWage",
+    "Issue": "Raise the minimum wage",
+    "For": "Photo_1059_01.png",
+    "Against": "Photo_1039_02.png"
   }
 ]
